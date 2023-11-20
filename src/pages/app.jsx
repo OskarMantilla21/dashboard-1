702c96eeaec4a1fe9e486b5947416f3b5b4ca509
OskarMantilla21 <137963398+OskarMantilla21@users.noreply.github.com>
2023-11-20 17:12:43 -0500

    se añadio el header y el menu

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
+import Menu from '../organism/menu'
+import Header from "../organism/header";
 import Layout from "../templates/layout";
+
+import "../styles/app.css";
 
 function App() {
   return (
     <div className="App">
       <Layout>
-
+        <Header/>
+        <Menu/>
       </Layout>
     </div>
   );
--- src/templates/layout.jsx
+++ src/templates/layout.jsx
@@ -3,16 +3,18 @@ import React from 'react';
 import  Grid  from '@mui/material/Unstable_Grid2';
 import  { Box, CssBaseline }  from '@mui/material';
 
-function Layout() {
+function Layout({children}) {
+const [header, Menu] = children;
+
   return (
     <Box style={{display: "flex", height: "100vh"}}>
       <CssBaseline />
       <Grid container sx={{flexGrow: 1}}>
         <Grid item flexBasis={220}>
-          Aside
+          {Menu}
         </Grid>
         <Grid item flex={1}> 
-          <Grid>Header</Grid>
+          <Grid xs={12}>{header}</Grid>
           <Grid container spacing={3}>
             <Grid xs={3}>PKI</Grid>
             <Grid xs={3}>PKI</Grid>
